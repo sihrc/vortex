ae68d4d18334a738e9465749812aa1ad7bf6a2e1
Chris Lee <sihrc.c.lee@gmail.com>
2018-08-04 17:22:07 -0700

    ADD: start_app method

--- vortex/app.py
+++ vortex/app.py
@@ -1,2 +1,22 @@
 from aiohttp import web
 
+DEFAULT_MIDDLEWARES = ()
+
+def start_app(
+    bind="0.0.0.0",
+    port=80,
+    route_managers=(),
+    middlewares=DEFAULT_MIDDLEWARES,
+    configs=None
+):
+    configs = configs or {}
+
+    app = web.Application()
+    app.update(**configs)
+
+    for route_manager in route_managers:
+        # TODO: Log route_manager routes loaded into app
+        app.add_subapp(route_manager.base, route_manager.app)
+
+    # TODO: Log server startup
+    web.run_app(app)
